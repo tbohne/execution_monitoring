5312362aa248a861582aab42282da92862bd28c6
tbohne <tbohne@uni-osnabrueck.de>
2021-11-01 10:20:02 +0000

    implement concurrency smach for execution monitoring

--- execution_monitoring/scripts/execution_monitoring.py
+++ execution_monitoring/scripts/execution_monitoring.py
@@ -20,6 +20,9 @@ class Contingency(smach.State):
         #   - if minor issue couldn't be handled / something went wrong: return "aggravated"
 
         # default for now:
+        for _ in range(10):
+            rospy.loginfo("#####################################################")
+            rospy.sleep(2)
         rospy.loginfo("contingency solved, continuing normal operation..")
         return "solved"
 
@@ -35,6 +38,9 @@ class Catastrophe(smach.State):
         #   - communicate problem
         #   - save state
         #   - ...
+        for _ in range(10):
+            rospy.loginfo("#####################################################")
+            rospy.sleep(2)
         rospy.loginfo("catastrophe processed, shutting down..")
         return "damage_control_performed"
 
@@ -60,8 +66,18 @@ class ExecutionMonitoringStateMachine(smach.StateMachine):
             output_keys=[]
         )
 
+        operation_monitoring = smach.Concurrence(outcomes=['minor_complication', 'critical_complication', 'end_of_episode'],
+                                                 default_outcome='end_of_episode',
+                                                 child_termination_cb=self.child_termination_callback,
+                                                 outcome_cb=self.out_callback)
+
+        with operation_monitoring:
+            smach.Concurrence.add('OPERATION', OperationStateMachine())
+            smach.Concurrence.add('CONTINGENCY_MONITORING', smach_ros.MonitorState("/contingency_preemption", String, self.monitoring_callback))
+            smach.Concurrence.add('CATASTROPHE_MONITORING', smach_ros.MonitorState("/catastrophe_preemption", String, self.monitoring_callback))
+
         with self:
-            self.add('NORMAL_OPERATION', OperationStateMachine(),
+            self.add('NORMAL_OPERATION', operation_monitoring,
                      transitions={'minor_complication': 'CONTINGENCY',
                                   'critical_complication': 'CATASTROPHE',
                                   'end_of_episode': 'SHUTDOWN'})
@@ -76,6 +92,42 @@ class ExecutionMonitoringStateMachine(smach.StateMachine):
             self.add('SHUTDOWN', Shutdown(),
                      transitions={'data_management_completed': 'system_deactivated'})
 
+    def child_termination_callback(self, outcome_map):
+        """
+        Called every time one of the child states terminates.
+
+        @return False: state machine should keep running
+                True:  preempt all remaining running states
+        """
+        if outcome_map['OPERATION'] in ['minor_complication', 'critical_complication', 'end_of_episode', 'preempted']:
+            return True
+        elif outcome_map['CONTINGENCY_MONITORING'] == 'invalid' or  outcome_map['CATASTROPHE_MONITORING'] == 'invalid':
+            return True
+        else:
+            return False
+
+    def out_callback(self, outcome_map):
+        """
+        Called once when the last child state terminates.
+
+        @return outcome of the concurrence state machine
+        """
+        if outcome_map['OPERATION'] == 'minor_complication' or outcome_map['CONTINGENCY_MONITORING'] == 'invalid':
+            return 'minor_complication'
+        elif outcome_map['OPERATION'] == 'critical_complication' or outcome_map['CATASTROPHE_MONITORING'] == 'invalid':
+            return 'critical_complication'
+        elif outcome_map['OPERATION'] == 'end_of_episode':
+            return 'end_of_episode'
+
+    def monitoring_callback(self, userdata, msg):
+        """
+        Needs to return False when we want the monitoring state to terminate.
+        """
+        rospy.loginfo("monitoring callback executed..")
+        rospy.loginfo("userdata: %s", userdata)
+        rospy.loginfo("msg: %s", msg)
+        return False
+
 
 def node():
     """
@@ -89,6 +141,7 @@ def node():
     outcome = sm.execute()
     rospy.loginfo("outcome: %s", outcome)
     rospy.spin()
+    sis.stop()
 
 
 if __name__ == '__main__':
