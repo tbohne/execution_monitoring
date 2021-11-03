d458cd0eb1031926669b047f30fc49f15403fdba
tbohne <tbohne@uni-osnabrueck.de>
2021-11-03 16:19:44 +0000

    complete dummy scanner action server

--- execution_monitoring/scripts/dummy_scanner.py
+++ execution_monitoring/scripts/dummy_scanner.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import rospy
 import actionlib
-from execution_monitoring.msg import ScanAction
+from execution_monitoring.msg import ScanAction, ScanResult
 
 class DummyScanner():
 
     def __init__(self):
+        rospy.loginfo("initializing dummy scanner -  waiting for tasks..")
         self.server = actionlib.SimpleActionServer('dummy_scanner', ScanAction, execute_cb=self.execute_cb, auto_start=False)
+        self.result = ScanResult()
         self.server.start()
         
     def execute_cb(self, goal):
-        rospy.loginfo("goal: %s", goal)
+        rospy.loginfo(goal)
         rospy.loginfo("start scanning procedure..")
         rospy.sleep(10)
-        self.server.set_succeeded(True)
+        self.result.result = "scanning successfully completed"
+        self.server.set_succeeded(self.result)
+        rospy.loginfo("scanning completed..")
 
 
 def node():
@@ -21,7 +25,7 @@ def node():
     Dummy node to simulate the scanning procedure, i.e. to scan on command.
     """
     rospy.init_node('dummy_scanner')
-
+    server = DummyScanner()
     rospy.spin()
 
 
