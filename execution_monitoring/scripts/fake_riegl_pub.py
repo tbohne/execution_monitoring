e90b7c315d12162dcac6d9e11a78c9eecd6c6bbd
tbohne <tbohne@uni-osnabrueck.de>
2021-11-04 10:51:29 +0000

    implement simulated sensor failure

--- execution_monitoring/scripts/fake_riegl_pub.py
+++ execution_monitoring/scripts/fake_riegl_pub.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String
 
 class FakeRiegl:
     """
@@ -10,8 +11,13 @@ class FakeRiegl:
     """
 
     def __init__(self):
+        self.simulate_sensor_failure = False
+        self.sensor_failure_sub = rospy.Subscriber("/simulate_sensor_failure", String, self.toggle_sensor_failure_callback, queue_size=1)
         self.scan_sub = rospy.Subscriber("/scanVelodyne", LaserScan, self.scan_callback, queue_size=1)
         self.scan_pub = rospy.Publisher("/RIEGL", LaserScan, queue_size=1)
+
+    def toggle_sensor_failure_callback(self, msg):
+        self.simulate_sensor_failure = not self.simulate_sensor_failure
 
     def scan_callback(self, scan):
         """
@@ -21,10 +27,12 @@ class FakeRiegl:
         """
         # rospy.loginfo("receiving laser scan..")
         # rospy.loginfo("seq: %s", scan.header.seq)
-        self.scan_pub.publish(scan)
+        if not self.simulate_sensor_failure:
+            self.scan_pub.publish(scan)
 
 def node():
     rospy.init_node('fake_riegl')
+    rospy.wait_for_message('SMACH_runnning', String)
     FakeRiegl()
     rospy.spin()
 
