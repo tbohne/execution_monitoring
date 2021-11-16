2c62d8d961d0c617f61a302f5cac412a209bc9b9
tbohne <tbohne@uni-osnabrueck.de>
2021-11-16 15:13:58 +0000

    implement fallback resolver / general structure of sensor failure resolver

--- execution_monitoring/scripts/resolver.py
+++ execution_monitoring/scripts/resolver.py
@@ -1,38 +1,112 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String, Bool
+from execution_monitoring import config
+
+
+class FallbackResolver:
+    """
+    The fallback resolver works in cases where no other solution is successful / available.
+    The fallback solution is to call the human operator who then takes care of the problem.
+    """
+
+    def __init__(self):
+        self.fallback_sub = rospy.Subscriber('/request_fallback', String, self.request_fallback, queue_size=1)
+        self.human_operator_sub = rospy.Subscriber('/problem_solved', String, self.solved_by_human_callback, queue_size=1)
+        self.human_operator_pub = rospy.Publisher("/request_help", String, queue_size=1)
+        self.fallback_pub = rospy.Publisher("/fallback_success", Bool, queue_size=1)
+        self.problem_resolved = False
+
+    def request_fallback(self, msg):
+        rospy.loginfo("fallback requested.. communicating problem to human operator..")
+        rospy.loginfo("problem: %s", msg.data)
+        self.human_operator_pub.publish(msg.data)
+
+        # TODO: when it takes too long it should go to CATASTROPHE
+        while not self.problem_resolved:
+            rospy.loginfo("waiting for human operator to solve the problem..")
+            rospy.sleep(5)
+        
+        rospy.loginfo("sensor failure resolved..")
+        self.fallback_pub.publish(True)
+
+    def solved_by_human_callback(self, msg):
+        rospy.loginfo("solved by human operator: %s", msg.data)
+        self.problem_resolved = True
+
 
 class SensorFailureResolver:
 
     def __init__(self):
         self.resolve_sub = rospy.Subscriber('/resolve_sensor_failure', String, self.resolve_callback, queue_size=1)
-        self.toggle_sim_sensor_failure_pub = rospy.Publisher("/toggle_simulated_sensor_failure", String, queue_size=1)
-        self.human_operator_pub = rospy.Publisher("/request_help", String, queue_size=1)
+        self.fallback_sub = rospy.Subscriber("/fallback_success", Bool, self.fallback_callback, queue_size=1)
+        self.toggle_sensor_failure_one_pub = rospy.Publisher("/toggle_simulated_sensor_failure", String, queue_size=1)
+        self.toggle_sensor_failure_two_pub = rospy.Publisher("/toggle_simulated_empty_ranges", String, queue_size=1)
+        self.toggle_sensor_failure_three_pub = rospy.Publisher("/toggle_simulated_look_to_sky", String, queue_size=1)
+        self.toggle_sensor_failure_four_pub = rospy.Publisher("/toggle_simulated_scan_repetition", String, queue_size=1)
         self.success_pub = rospy.Publisher('/resolve_sensor_failure_success', Bool, queue_size=1)
-        self.human_operator_sub = rospy.Subscriber('/problem_solved', String, self.solved_by_human_callback, queue_size=1)
+        self.fallback_pub = rospy.Publisher('/request_fallback', String, queue_size=1)
         self.problem_resolved = False
+
+    def resolve_type_one_failure(self, msg):
+        rospy.loginfo("resolve type one failure..")
+        self.fallback_pub.publish(msg)
+        while not self.problem_resolved:
+            rospy.sleep(5)
+        self.toggle_sensor_failure_one_pub.publish("")
+
+    def resolve_type_two_failure(self, msg):
+        rospy.loginfo("resolve type two failure..")
+        self.fallback_pub.publish(msg)
+        while not self.problem_resolved:
+            rospy.sleep(5)
+        self.toggle_sensor_failure_two_pub.publish("")
+
+    def resolve_type_three_failure(self, msg):
+        rospy.loginfo("resolve type three failure..")
+        self.fallback_pub.publish(msg)
+        while not self.problem_resolved:
+            rospy.sleep(5)
+        self.toggle_sensor_failure_three_pub.publish("")
+
+    def resolve_type_four_failure(self, msg):
+        rospy.loginfo("resolve type four failure..")
+        self.fallback_pub.publish(msg)
+        while not self.problem_resolved:
+            rospy.sleep(5)
+        self.toggle_sensor_failure_four_pub.publish("")
+
+    def fallback_callback(self, msg):
+        if msg.data:
+            rospy.loginfo("solved by fallback resolver: %s", msg.data)
+            self.problem_resolved = True
+        else:
+            rospy.loginfo("fallback solution was not successful..")
 
     def resolve_callback(self, msg):
         rospy.loginfo("launch sensor failure resolver..")
+        rospy.loginfo("type of sensor failure: %s", msg.data)
         self.problem_resolved = False
-        rospy.loginfo("not able to handle - communicating problem to human operator..")
-        self.human_operator_pub.publish("sensor failure detected - preempted NORMAL_OPERATION - need help")
 
-        # TODO: when it takes too long it should go to CATASTROPHE
-        while not self.problem_resolved:
-            rospy.loginfo("waiting for human operator to solve the problem..")
-            rospy.sleep(30)
-        self.toggle_sim_sensor_failure_pub.publish("")
-        self.success_pub.publish(True)
-        rospy.loginfo("sensor failure resolved..")
+        # different types of resolution are required based on the type of issue
+        if msg.data == config.SENSOR_FAILURE_ONE:
+            self.resolve_type_one_failure(config.SENSOR_FAILURE_ONE)
+        elif msg.data == config.SENSOR_FAILURE_TWO:
+            self.resolve_type_two_failure(config.SENSOR_FAILURE_TWO)
+        elif msg.data == config.SENSOR_FAILURE_THREE:
+            self.resolve_type_three_failure(config.SENSOR_FAILURE_THREE)
+        elif msg.data == config.SENSOR_FAILURE_FOUR:
+            self.resolve_type_four_failure(config.SENSOR_FAILURE_FOUR)
 
-    def solved_by_human_callback(self, msg):
-        self.problem_resolved = True
+        if self.problem_resolved:
+            self.success_pub.publish(True)
+    
 
 def node():
     rospy.init_node('failure_resolver')
     rospy.wait_for_message('SMACH_runnning', String)
     SensorFailureResolver()
+    FallbackResolver()
     rospy.spin()
 
 if __name__ == '__main__':
