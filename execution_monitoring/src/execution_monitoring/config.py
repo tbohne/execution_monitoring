9e2260941ba7a1e725e7d5c38624252cc9f822fd
tbohne <tbohne@uni-osnabrueck.de>
2021-11-19 12:02:25 +0000

    extend config / implement util parser

--- execution_monitoring/src/execution_monitoring/config.py
+++ execution_monitoring/src/execution_monitoring/config.py
@@ -4,3 +4,6 @@ SENSOR_FAILURE_ONE = "total sensor failure"
 SENSOR_FAILURE_TWO = "empty list of range values"
 SENSOR_FAILURE_THREE = "look to the sky"
 SENSOR_FAILURE_FOUR = "repeated scan"
+
+SCAN_PATH = "/home/docker/catkin_ws/src/execution_monitoring/execution_monitoring/scans/"
+SCAN_TIME_LIMIT = 60
--- execution_monitoring/src/execution_monitoring/util.py
+++ execution_monitoring/src/execution_monitoring/util.py
@@ -1,1 +1,4 @@
 #!/usr/bin/env python
+
+def parse_mission_name(mission_name):
+    return mission_name.data.strip().replace(" ", "_").replace(",", "_").replace("__", "_").replace(":", "_")
