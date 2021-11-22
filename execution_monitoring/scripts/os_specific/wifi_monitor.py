a1d3e8bb60aad696dee166b9678bbe7b756e180f
tbohne <tbohne@uni-osnabrueck.de>
2021-11-22 20:11:02 +0000

    implement wifi failure simulations

--- execution_monitoring/scripts/os_specific/wifi_monitor.py
+++ execution_monitoring/scripts/os_specific/wifi_monitor.py
@@ -5,6 +5,7 @@ import re
 import datetime
 from execution_monitoring.msg import WiFi
 from execution_monitoring import config
+from std_msgs.msg import String
 
 class WiFiMonitor:
     """
@@ -20,15 +21,38 @@ class WiFiMonitor:
             - -70 dBm good
             - -80 dBm low
             - -90 dBm ver low
-            - -100 dBm no signal       
+            - -100 dBm no signal
         - bit_rate (Mb/s): speed at which bits are transmitted over the medium
 
     If the wifi is not connected at all, such an OS-specific wifi connection monitoring node is expected to set all three values to 0.
     """
 
     def __init__(self):
+
+        self.simulate_bad_wifi_link = False
+        self.simulate_bad_wifi_signal = False
+        self.simulate_bad_wifi_bit_rate = False
+        self.simulate_wifi_disconnect = False
+
+        rospy.Subscriber("/toggle_simulated_bad_wifi_link", String, self.toggle_bad_wifi_link_callback, queue_size=1)
+        rospy.Subscriber("/toggle_simulated_bad_wifi_signal", String, self.toggle_bad_wifi_signal_callback, queue_size=1)
+        rospy.Subscriber("/toggle_simulated_bad_wifi_bit_rate", String, self.toggle_bad_wifi_bit_rate_callback, queue_size=1)
+        rospy.Subscriber("/toggle_simulated_wifi_disconnect", String, self.toggle_wifi_disconnect_callback, queue_size=1)
+
         self.wifi_info_pub = rospy.Publisher('/wifi_connectivity_info', WiFi, queue_size=1)
         self.monitor_wifi()
+
+    def toggle_bad_wifi_link_callback(self, msg):
+        self.simulate_bad_wifi_link = not self.simulate_bad_wifi_link
+
+    def toggle_bad_wifi_signal_callback(self, msg):
+        self.simulate_bad_wifi_signal = not self.simulate_bad_wifi_signal
+
+    def toggle_bad_wifi_bit_rate_callback(self, msg):
+        self.simulate_bad_wifi_bit_rate = not self.simulate_bad_wifi_bit_rate
+
+    def toggle_wifi_disconnect_callback(self, msg):
+        self.simulate_wifi_disconnect = not self.simulate_wifi_disconnect
 
     def now(self):
         return datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -57,6 +81,16 @@ class WiFiMonitor:
             wifi_msg.link_quality = link_quality
             wifi_msg.signal_level = signal_level
             wifi_msg.bit_rate = bit_rate
+
+            if self.simulate_wifi_disconnect:
+                wifi_msg.link_quality = wifi_msg.signal_level = wifi_msg.bit_rate = config.WIFI_DISCONNECT
+            if self.simulate_bad_wifi_link:
+                wifi_msg.link_quality = config.BAD_WIFI_LINK_QUALITY
+            if self.simulate_bad_wifi_signal:
+                wifi_msg.signal_level = config.BAD_WIFI_SIGNAL_LEVEL
+            if self.simulate_bad_wifi_bit_rate:
+                wifi_msg.bit_rate = config.BAD_WIFI_BIT_RATE
+
             self.wifi_info_pub.publish(wifi_msg)
             rospy.sleep(10)
 
