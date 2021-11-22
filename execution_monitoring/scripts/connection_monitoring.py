19ad1dc324833b077f6940b71d07f3c3eae56fad
tbohne <tbohne@uni-osnabrueck.de>
2021-11-22 15:59:53 +0000

    communicate wifi info into ROS world

--- execution_monitoring/scripts/connection_monitoring.py
+++ execution_monitoring/scripts/connection_monitoring.py
@@ -7,7 +7,7 @@ from execution_monitoring import util, config
 class ConnectionMonitoring:
 
     def __init__(self):
-        self.link_quality_sub = rospy.Subscriber('/wifi_link_quality', WiFi, self.wifi_callback, queue_size=1)
+        self.wifi_info_sub = rospy.Subscriber('/wifi_connectivity_info', WiFi, self.wifi_callback, queue_size=1)
 
     def wifi_callback(self, wifi_info):
         rospy.loginfo("receiving new wifi info..")
@@ -15,7 +15,7 @@ class ConnectionMonitoring:
 
 def node():
     rospy.init_node('connection_monitoring')
-    rospy.wait_for_message('SMACH_runnning', String)
+    # rospy.wait_for_message('SMACH_runnning', String)
     rospy.loginfo("launch connection monitoring..")
     ConnectionMonitoring()
     rospy.spin()
--- execution_monitoring/scripts/os_specific/wifi_monitor.py
+++ execution_monitoring/scripts/os_specific/wifi_monitor.py
@@ -3,32 +3,44 @@ import rospy
 import subprocess
 import re
 import datetime
+from execution_monitoring.msg import WiFi
 from execution_monitoring import config
 
-def now():
-    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+class WiFiMonitor:
 
-def parse_relevant_parameters(iwconfig_input):
-    first = re.search(r"Link Quality.*", iwconfig_input).group(0).strip().split("  ")
-    sec = re.search(r"Bit Rate.*", iwconfig_input).group(0).strip().split("  ")
-    link_quality, link_quality_UB = first[0].split("=")[1].split("/")
-    link_quality_percentage = round(float(link_quality) / float(link_quality_UB) * 100, 2)
-    signal_level = int(first[1].split("=")[1].split(" ")[0])
-    bit_rate = float(sec[0].split("=")[1].split(" ")[0])
-    return link_quality_percentage, signal_level, bit_rate
+    def __init__(self):
+        self.wifi_info_pub = rospy.Publisher('/wifi_connectivity_info', WiFi, queue_size=1)
+        self.monitor_wifi()
 
-def monitor_wifi():
+    def now(self):
+        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 
-    while not rospy.is_shutdown():
-        p = subprocess.Popen(["iwconfig", config.WIFI_INTERFACE], stdout=subprocess.PIPE)
-        out, err = p.communicate()
-        link_quality, signal_level, bit_rate = parse_relevant_parameters(out)
-        rospy.loginfo("%s --- link quality: %s%%, signal level: %s dBm, bit rate: %s Mb/s", now(), link_quality, signal_level, bit_rate)
-        rospy.sleep(10)
+    def parse_relevant_parameters(selfl, iwconfig_input):
+        first = re.search(r"Link Quality.*", iwconfig_input).group(0).strip().split("  ")
+        sec = re.search(r"Bit Rate.*", iwconfig_input).group(0).strip().split("  ")
+        link_quality, link_quality_UB = first[0].split("=")[1].split("/")
+        link_quality_percentage = round(float(link_quality) / float(link_quality_UB) * 100, 2)
+        signal_level = int(first[1].split("=")[1].split(" ")[0])
+        bit_rate = float(sec[0].split("=")[1].split(" ")[0])
+        return link_quality_percentage, signal_level, bit_rate
+
+    def monitor_wifi(self):
+        while not rospy.is_shutdown():
+            p = subprocess.Popen(["iwconfig", config.WIFI_INTERFACE], stdout=subprocess.PIPE)
+            out, err = p.communicate()
+            link_quality, signal_level, bit_rate = self.parse_relevant_parameters(out)
+            rospy.loginfo("%s --- link quality: %s%%, signal level: %s dBm, bit rate: %s Mb/s", self.now(), link_quality, signal_level, bit_rate)
+
+            wifi_msg = WiFi()
+            wifi_msg.link_quality = link_quality
+            wifi_msg.signal_level = signal_level
+            wifi_msg.bit_rate = bit_rate
+            self.wifi_info_pub.publish(wifi_msg)
+            rospy.sleep(10)
 
 def node():
     rospy.init_node('wifi_monitor')
-    monitor_wifi()
+    WiFiMonitor()
     rospy.spin()
 
 if __name__ == '__main__':
