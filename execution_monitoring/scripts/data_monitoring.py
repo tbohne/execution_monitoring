54b2d21f6aa357a4aaf51e936d9e0296180bc018
tbohne <tbohne@uni-osnabrueck.de>
2021-11-24 14:30:25 +0000

    refine data monitoring

--- execution_monitoring/scripts/data_monitoring.py
+++ execution_monitoring/scripts/data_monitoring.py
@@ -13,33 +13,35 @@ class DataMonitoring:
         self.scan_action_sub = rospy.Subscriber('/scan_action', String, self.data_management_failure_monitoring, queue_size=1)
         self.mission_name_sub = rospy.Subscriber('/mission_name', String, self.mission_name_callback, queue_size=1)
 
-    # def specific_scan_check(self):
-    #     log_file = Path(config.SCAN_PATH + self.mission_name + config.SCAN_FILE_EXTENSION)
-    #     scan_cnt_before = 0
-    #     if log_file.is_file():
-    #         rospy.loginfo("reading file before scan logging..")
-    #         scan_cnt_before = self.count_scan_entries()
+    def specific_scan_check(self):
+        log_file = Path(config.SCAN_PATH + self.mission_name + config.SCAN_FILE_EXTENSION)
+        scan_cnt_before = 0
+        if log_file.is_file():
+            rospy.loginfo("reading file before scan logging..")
+            scan_cnt_before = self.count_scan_entries()
         
-    #     # TODO: should actually be the time limit for scanning
-    #     rospy.sleep(10)
+        rospy.sleep(config.SCAN_TIME_LIMIT)
+        rospy.loginfo("reading file after scan logging..")
+        scan_cnt_after = self.count_scan_entries()
 
-    #     rospy.loginfo("reading file after scan logging..")
-    #     scan_cnt_after = self.count_scan_entries()
-
-    #     if scan_cnt_after != scan_cnt_before + 1:
-    #         rospy.loginfo("data management error..")
-    #         self.contingency_pub.publish(config.DATA_MANAGEMENT_FAILURE_TWO)
-    #     else:
-    #         rospy.loginfo("data management OK - scan successfully logged..")
+        if scan_cnt_after != scan_cnt_before + 1:
+            rospy.loginfo("data management error..")
+            self.contingency_pub.publish(config.DATA_MANAGEMENT_FAILURE_TWO)
+        else:
+            rospy.loginfo("data management OK - scan successfully logged..")
     
-    # def count_scan_entries(self):
-    #     scan_cnt = 0
-    #     with open(config.SCAN_PATH + self.mission_name + config.SCAN_FILE_EXTENSION, 'r') as scan_log_file:
-    #         for l in scan_log_file.readlines():
-    #             # new scan begins
-    #             if "header" in l:
-    #                 scan_cnt += 1
-    #     return scan_cnt
+    def count_scan_entries(self):
+        scan_cnt = 0
+        try:
+            with open(config.SCAN_PATH + self.mission_name + config.SCAN_FILE_EXTENSION, 'r') as scan_log_file:
+                for l in scan_log_file.readlines():
+                    # new scan begins
+                    if "header" in l:
+                        scan_cnt += 1
+        except Exception as e:
+            rospy.loginfo("unable to read scan log: %s", e)
+            return -1
+        return scan_cnt
 
     def drive_capacity_check(self):
         disk_use = psutil.disk_usage(config.MONITOR_DRIVE)
@@ -54,12 +56,11 @@ class DataMonitoring:
             rospy.loginfo("drive capacity: %s", str(disk_use.total / (1024.0 ** 3)) + " GB")
             rospy.loginfo("percentage used: %s", disk_use.percent)
 
-    # TODO: pretty specific what happens there, should be part of an optional specific module
     def data_management_failure_monitoring(self, msg):
         rospy.loginfo("start data management monitoring..")
         self.drive_capacity_check()
-        # TODO: should only take place AFTER successful sensor monitoring
-        #self.specific_scan_check()
+        if config.ENABLE_SPECIFIC_LASER_SCAN_CHECK:
+            self.specific_scan_check()
 
     def mission_name_callback(self, mission_name):
         self.mission_name = util.parse_mission_name(mission_name)
