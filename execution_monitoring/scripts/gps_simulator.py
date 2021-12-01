4324f99b6b6853985ef3eedf2c812203d1cc62a3
tbohne <tbohne@uni-osnabrueck.de>
2021-12-01 17:10:08 +0000

    extend GNSS sim

--- execution_monitoring/scripts/gps_simulator.py
+++ execution_monitoring/scripts/gps_simulator.py
@@ -6,14 +6,24 @@ from sensor_msgs.msg import NavSatFix
 from std_msgs.msg import String
 
 class GPSSimulator:
+    """
+    Simple node that receives the simulated GPS data from libhector_gazebo_ros_gps and acts as a "man-in-the-middle".
+    Enriches the data published by libhector_gazebo_ros_gps with further information.
+    """
 
     def __init__(self):
         # subscribe to topic of quadrotor_gps_sim (libhector_gazebo_ros_gps.so)
         #    -> gazebo plugin that simulates GPS data
         rospy.Subscriber('/fix_plugin', NavSatFix, self.sim_gps_callback, queue_size=1)
 
-    def sim_gps_callback(self, msg):
-        rospy.loginfo("receiving gps: %s", msg)
+    def sim_gps_callback(self, nav_sat_fix):
+        rospy.loginfo("receiving gps: %s", nav_sat_fix)
+        nav_sat_fix.status.status = config.GNSS_STATUS
+        nav_sat_fix.status.service = config.GNSS_SERVICE
+        nav_sat_fix.position_covariance = config.GNSS_COVARIANCES
+        nav_sat_fix.position_covariance_type = config.GNSS_COVARIANCE_TYPE
+        rospy.loginfo("receiving gps: %s", nav_sat_fix)
+
 
 def node():
     rospy.init_node('gps_simulator')
