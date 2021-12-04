c887c3e49fcfa2e66dda918f14a4de0dfd543f93
tbohne <tbohne@uni-osnabrueck.de>
2021-12-04 13:57:28 +0000

    simulate GNSS data of different quality

--- execution_monitoring/scripts/gnss_simulator.py
+++ execution_monitoring/scripts/gnss_simulator.py
@@ -16,12 +16,30 @@ class GNSSSimulator:
         self.gnss_sub = rospy.Subscriber('/fix_plugin', NavSatFix, self.sim_gps_callback, queue_size=1)
 
         rospy.Subscriber("/toggle_simulated_timeout_failure", String, self.toggle_timeout_callback, queue_size=1)
+        rospy.Subscriber("/set_simulated_good_quality", String, self.set_good_qualitiy_callback, queue_size=1)
+        rospy.Subscriber("/set_simulated_med_quality", String, self.set_med_qualitiy_callback, queue_size=1)
+        rospy.Subscriber("/set_simulated_low_quality", String, self.set_low_qualitiy_callback, queue_size=1)
         self.sim_timeout = False
+        self.sim_good_quality = True
+        self.sim_med_quality = False
+        self.sim_low_quality = False
 
         self.gps_publisher = rospy.Publisher('/fix', NavSatFix, queue_size=1)
 
     def toggle_timeout_callback(self, msg):
         self.sim_timeout = not self.sim_timeout
+
+    def set_good_qualitiy_callback(self, msg):
+        self.sim_med_quality = self.sim_low_quality = False
+        self.sim_good_quality = True
+
+    def set_med_qualitiy_callback(self, msg):
+        self.sim_good_quality = self.sim_low_quality = False
+        self.sim_med_quality = True
+
+    def set_low_qualitiy_callback(self, msg):
+        self.sim_good_quality = self.sim_med_quality = False
+        self.sim_low_quality = True
 
     def sim_gps_callback(self, nav_sat_fix):
         #rospy.loginfo("receiving GNSS data: %s", nav_sat_fix)
@@ -32,10 +50,21 @@ class GNSSSimulator:
             self.sim_timeout = False
             self.gnss_sub = rospy.Subscriber('/fix_plugin', NavSatFix, self.sim_gps_callback, queue_size=1)
 
-        nav_sat_fix.status.status = config.GNSS_STATUS
-        nav_sat_fix.status.service = config.GNSS_SERVICE
-        nav_sat_fix.position_covariance = config.GNSS_COVARIANCES
-        nav_sat_fix.position_covariance_type = config.GNSS_COVARIANCE_TYPE
+        if self.sim_good_quality:
+            nav_sat_fix.status.status = config.GNSS_STATUS_GBAS_FIX
+            nav_sat_fix.position_covariance_type = config.GNSS_COVARIANCE_TYPE_DIAGONAL_KNOWN
+            nav_sat_fix.position_covariance = [3.5, 0.0, 0.0, 0.0, 5.7, 0.0, 0.9, 0.9, 9.9]
+            nav_sat_fix.status.service = config.GNSS_SERVICE_GPS
+        elif self.sim_med_quality:
+            nav_sat_fix.status.status = config.GNSS_STATUS_FIX
+            nav_sat_fix.position_covariance_type = config.GNSS_COVARIANCE_TYPE_APPROXIMATED
+            nav_sat_fix.position_covariance = [3.5, 0.0, 0.0, 0.0, 5.7, 0.0, 0.9, 0.9, 9.9]
+            nav_sat_fix.status.service = config.GNSS_SERVICE_GALILEO
+        elif self.sim_low_quality:
+            nav_sat_fix.status.status = config.GNSS_STATUS_FIX
+            nav_sat_fix.position_covariance_type = config.GNSS_COVARIANCE_TYPE_UNKNOWN
+            nav_sat_fix.status.service = config.GNSS_SERVICE_GALILEO
+
         self.gps_publisher.publish(nav_sat_fix)
 
 def node():
