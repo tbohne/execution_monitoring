3f69915265666d150b01ac63b8435c6adc5d290a
tbohne <tbohne@uni-osnabrueck.de>
2021-12-07 15:07:33 +0000

    add API connection check + location info

--- execution_monitoring/scripts/weather_monitoring.py
+++ execution_monitoring/scripts/weather_monitoring.py
@@ -16,28 +16,30 @@ class WeatherMonitoring:
     def launch_weather_monitoring(self):
         
         owm = OWM(secret_config.OWM_API_KEY)
-        observation = owm.weather_at_place('Osnabrueck,DE')
-        w = observation.get_weather()
 
-        rospy.loginfo("time of observation: %s", w.get_reference_time(timeformat='iso'))
-        rospy.loginfo("status: %s", w.get_detailed_status())
-        rospy.loginfo("cloudiness percentage: %s", w.get_clouds())
-        rospy.loginfo("rain volume for the last 3 hours: %s mm", w.get_rain())
-        rospy.loginfo("snow volume for the last 3 hours: %s mm", w.get_snow())
-        rospy.loginfo("wind: %s", w.get_wind())
-        rospy.loginfo("humidity percentage: %s", w.get_humidity())
-        rospy.loginfo("atmospheric pressure: %s", w.get_pressure())
-        rospy.loginfo("temperature (degrees C): %s", w.get_temperature('celsius'))
-        rospy.loginfo("OWM weather condition code: %s", w.get_weather_code())
-        rospy.loginfo("weather-related icon name: %s", w.get_weather_icon_name())
-        rospy.loginfo("sunrise time: %s", w.get_sunrise_time('iso'))
-        rospy.loginfo("sunset time: %s", w.get_sunset_time('iso'))
+        if owm.is_API_online():
+            observation = owm.weather_at_place('Osnabrueck,DE')
+            w = observation.get_weather()
+            print("location: " + observation.get_location().get_name())
+            rospy.loginfo("time of observation: %s", w.get_reference_time(timeformat='iso'))
+            rospy.loginfo("status: %s", w.get_detailed_status())
+            rospy.loginfo("cloudiness percentage: %s", w.get_clouds())
+            rospy.loginfo("rain volume for the last 3 hours: %s mm", w.get_rain())
+            rospy.loginfo("snow volume for the last 3 hours: %s mm", w.get_snow())
+            rospy.loginfo("wind: %s", w.get_wind())
+            rospy.loginfo("humidity percentage: %s", w.get_humidity())
+            rospy.loginfo("atmospheric pressure: %s", w.get_pressure())
+            rospy.loginfo("temperature (degrees C): %s", w.get_temperature('celsius'))
+            rospy.loginfo("OWM weather condition code: %s", w.get_weather_code())
+            rospy.loginfo("weather-related icon name: %s", w.get_weather_icon_name())
+            rospy.loginfo("sunrise time: %s", w.get_sunrise_time('iso'))
+            rospy.loginfo("sunset time: %s", w.get_sunset_time('iso'))
 
-        fc = owm.three_hours_forecast('Osnabrueck,DE')
-        f = fc.get_forecast().get_weathers()
-        rospy.loginfo("forecast for the next few hours:")
-        for i in range(0, 3):
-            rospy.loginfo("%s, status: %s", f[i].get_reference_time('iso'), f[i].get_status())
+            fc = owm.three_hours_forecast('Osnabrueck,DE')
+            f = fc.get_forecast().get_weathers()
+            rospy.loginfo("forecast for the next few hours:")
+            for i in range(0, 3):
+                rospy.loginfo("%s, status: %s", f[i].get_reference_time('iso'), f[i].get_status())
 
 
 def node():
