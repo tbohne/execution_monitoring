fa5da2faa4faec5f3aeb8736cb0582ea4ae5e2d9
tbohne <tbohne@uni-osnabrueck.de>
2022-02-09 20:22:25 +0000

    impl monitoring for explicit docking fail

--- execution_monitoring/scripts/charging_failure_monitoring.py
+++ execution_monitoring/scripts/charging_failure_monitoring.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from execution_monitoring import config
 
 class ChargingFailureMonitoring:
 
     def __init__(self):
         rospy.Subscriber('/explicit_charging_failure', String, self.explicit_failure_callback, queue_size=1)
 
+        self.contingency_pub = rospy.Publisher('/contingency_preemption', String, queue_size=1)
+
     def explicit_failure_callback(self, msg):
         rospy.loginfo("explicit charging fail communicated by operation smach..")
-        pass
+        self.contingency_pub.publish(config.CHARGING_FAILURE_ONE)
 
 def node():
     rospy.init_node('charging_failure_monitoring')
