dd3ec55e7c8ac0ad18748e6a13a8a3364f3253fd
tbohne <tbohne@uni-osnabrueck.de>
2022-02-10 08:26:00 +0000

    sim undocking fail

--- execution_monitoring/scripts/charging_failure_monitoring.py
+++ execution_monitoring/scripts/charging_failure_monitoring.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String, Float64
 from execution_monitoring import config
 
 class ChargingFailureMonitoring:
 
     def __init__(self):
         rospy.Subscriber('/explicit_charging_failure', String, self.explicit_failure_callback, queue_size=1)
+        rospy.Subscriber('/undocking_fail_sim', String, self.undocking_fail_callback, queue_size=1)
 
         self.contingency_pub = rospy.Publisher('/contingency_preemption', String, queue_size=1)
 
+    def undocking_fail_callback(self, msg):
+        # TODO: should be event-based in experiments later -- close container just before undocking starts
+        rospy.loginfo("sim undocking fail -- sending command to close container front..")
+        container_pub = rospy.Publisher('/container/rampB_position_controller/command', Float64, queue_size=1)
+        for _ in range(3):
+            container_pub.publish(-2.0)
+            rospy.sleep(0.5)
+
     def explicit_failure_callback(self, msg):
         rospy.loginfo("explicit charging fail communicated by operation smach..")
-        self.contingency_pub.publish(config.CHARGING_FAILURE_ONE)
+        self.contingency_pub.publish(msg.data)
 
 def node():
     rospy.init_node('charging_failure_monitoring')
