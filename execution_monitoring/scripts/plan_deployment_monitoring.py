bbe9e727f1aad8c46e180b83fac606b0a32083b0
tbohne <tbohne@uni-osnabrueck.de>
2022-02-17 12:54:35 +0100

    minor changes

--- execution_monitoring/scripts/plan_deployment_monitoring.py
+++ execution_monitoring/scripts/plan_deployment_monitoring.py
@@ -29,7 +29,7 @@ class PlanDeploymentMonitor:
 
     def operation_callback(self, operation_state):
         rospy.loginfo("state of operation: %s", operation_state)
-        self.robot_info_pub.publish("state of operation: " + operation_state)
+        self.robot_info_pub.publish("state of operation: " + str(operation_state))
         self.time_since_last_op = datetime.now()
     
     def plan_fail_callback(self, msg):
