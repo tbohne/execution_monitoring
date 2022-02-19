e0c20f6108eb26258b35227288ad3e46a8fea251
tbohne <tbohne@uni-osnabrueck.de>
2022-02-19 16:24:32 +0100

    refine PM monitoring

--- execution_monitoring/scripts/power_management_monitoring.py
+++ execution_monitoring/scripts/power_management_monitoring.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String, Float64
+from std_msgs.msg import String, Float64, Bool
 from execution_monitoring import config
 from arox_performance_parameters.msg import arox_battery_params
 
 class PowerManagementMonitoring:
 
     def __init__(self):
+        self.active_catastrophe_monitoring = True
+        self.active_contingency_monitoring = True
         self.contingency_pub = rospy.Publisher('/contingency_preemption', String, queue_size=1)
         self.catastrophe_pub = rospy.Publisher('/catastrophe_preemption', String, queue_size=1)
         rospy.Subscriber('/watchdog', String, self.watchdog_callback, queue_size=1)
+        rospy.Subscriber('/resolve_power_management_failure_success', Bool, self.resolve_callback, queue_size=1)
+
+    def resolve_callback(self, res):
+        self.active_catastrophe_monitoring = True
+        self.active_contingency_monitoring = True
 
     def watchdog_callback(self, msg):
-        if msg.data == config.CONTINGENCY_MSG:
+        if self.active_contingency_monitoring and msg.data == config.CONTINGENCY_MSG:
             rospy.loginfo("battery watchdog module signals contingency -- initiate contigency")
             rospy.loginfo(config.POWER_MANAGEMENT_FAILURE_ONE)
             self.contingency_pub.publish(config.POWER_MANAGEMENT_FAILURE_ONE)
-        elif msg.data == config.CATASTROPHE_MSG:
+            self.active_contingency_monitoring = False
+        elif self.active_catastrophe_monitoring and msg.data == config.CATASTROPHE_MSG:
             rospy.loginfo("battery watchdog module signals catastrophe -- initiate catastrophe")
             rospy.loginfo(config.POWER_MANAGEMENT_FAILURE_TWO)
             self.catastrophe_pub.publish(config.POWER_MANAGEMENT_FAILURE_TWO)
+            self.active_catastrophe_monitoring = False
 
 def node():
     rospy.init_node('power_management_monitoring')
