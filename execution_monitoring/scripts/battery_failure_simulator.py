e4667f9a9a45b516ed3cb8806657ea2cffc21e58
tbohne <tbohne@uni-osnabrueck.de>
2022-02-21 13:16:57 +0100

    discharge reset - case differentiation

--- execution_monitoring/scripts/battery_failure_simulator.py
+++ execution_monitoring/scripts/battery_failure_simulator.py
@@ -12,17 +12,27 @@ class BatteryFailureSimulator:
         rospy.Subscriber('/sim_power_management_catastrophe', String, self.sim_catastrophe_callback, queue_size=1)
         rospy.Subscriber('/reset_discharge_rate', String, self.reset_discharge_rate_callback, queue_size=1)
         self.client = dynamic_reconfigure.client.Client("arox_battery", timeout=30)
+        self.sim_contingency = False
+        self.sim_catastrophe = False
 
     def reset_discharge_rate_callback(self, msg):
-        rospy.loginfo("resetting discharge rate (back to normal)..")
-        self.client.update_configuration({"discharge_rate": config.NORMAL_DISCHARGE_RATE})
+        if msg == config.POWER_MANAGEMENT_FAILURE_ONE and self.sim_contingency:
+            rospy.loginfo("resetting discharge rate (back to normal)..")
+            self.sim_contingency = False
+            self.client.update_configuration({"discharge_rate": config.NORMAL_DISCHARGE_RATE})
+        elif msg == config.POWER_MANAGEMENT_FAILURE_TWO and self.sim_catastrophe:
+            rospy.loginfo("resetting discharge rate (back to normal)..")
+            self.sim_catastrophe = False
+            self.client.update_configuration({"discharge_rate": config.NORMAL_DISCHARGE_RATE})
 
     def sim_contingency_callback(self, msg):
         rospy.loginfo("sim PM contingency..")
+        self.sim_contingency = True
         self.client.update_configuration({"discharge_rate": config.CONTINGENCY_DISCHARGE_RATE})
 
     def sim_catastrophe_callback(self, msg):
         rospy.loginfo("sim PM catastrohe..")
+        self.sim_catastrophe = True
         self.client.update_configuration({"discharge_rate": config.CATASTROPHE_DISCHARGE_RATE})
 
 def node():
