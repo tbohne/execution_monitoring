b7776e7252da6fef04c5d3b3555946f0745ab520
tbohne <tbohne@uni-osnabrueck.de>
2022-03-12 20:27:33 +0100

    fix transformation + sim problem counter

--- execution_monitoring/scripts/experiments.py
+++ execution_monitoring/scripts/experiments.py
@@ -3,12 +3,12 @@ import rospy
 from datetime import datetime
 import random
 from std_msgs.msg import String
-from execution_monitoring import config
+from execution_monitoring import config, util
 from arox_performance_parameters.msg import arox_operational_param
 from arox_performance_parameters.msg import arox_battery_params
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
-import tf
+import tf2_ros
 import math
 
 # would break the mission -- it's a cata in 100% of the cases
@@ -71,6 +71,7 @@ EXPERIMENT_DURATION = 18000 # 14400 # 4 hours
 IDX = 0
 SIM_FAILURES = True
 PLAN_LENGTH = 34
+TF_BUFFER = None
 
 class Experiment:
 
@@ -99,6 +100,7 @@ class Experiment:
         self.prev_start = None
         self.prev_pose = None
         self.arox_total_dist = 0
+        self.simulated_problems = 0
 
         rospy.Subscriber("/contingency_preemption", String, self.contingency_callback)
         rospy.Subscriber("/catastrophe_preemption", String, self.catastrophe_callback)
@@ -116,11 +118,9 @@ class Experiment:
         curr_pose.pose.position.y = msg.pose.pose.position.y
         curr_pose.pose.orientation.z = msg.pose.pose.orientation.z
         curr_pose.pose.orientation.w = msg.pose.pose.orientation.w
-        tf_listener = tf.TransformListener()
             
         try:
-            tf_listener.waitForTransform(target_frame="map", source_frame=curr_pose.header.frame_id, time=rospy.Time(0), timeout=rospy.Duration(10.0))
-            curr_pose_map = tf_listener.transformPose("map", curr_pose)
+            curr_pose_map = util.transform_pose(TF_BUFFER, curr_pose, 'map')
 
             if not self.prev_pose:
                 self.prev_pose = curr_pose_map
@@ -130,12 +130,14 @@ class Experiment:
 
                 self.arox_total_dist += dist
                 self.prev_pose = curr_pose_map
+                
         except Exception as e:
             rospy.loginfo("ERROR: %s", e)
 
     def sim_info_callback(self, msg):
         # by this, we know when there is a msg on a fail sim topic and thus when a contingency is expected
         rospy.loginfo("SIM LAUNCHED --------------------------")
+        self.simulated_problems += 1
         self.sim_launched = True
         # reset timer -- sim only launched now -- next shouldn't follow immediately, e.g. docking
         self.sim_fail_time = datetime.now()
@@ -259,12 +261,12 @@ class Experiment:
         try:
             with open(config.EXP_PATH + "results.csv", 'a') as out_file:
                 if IDX == 0:
-                    out_file.write("experiment,duration,correct_contingencies,false_positives,false_negatives,correct_no_contingency,unexpected_contingencies,completed,completed_tasks,charge_cycles,mission_cycles,total_dist,traverse_time,scan_time,wait_time,cata_time,cont_time,charge_time,dock_time,undock_time\n")
+                    out_file.write("experiment,duration,correct_contingencies,false_positives,false_negatives,correct_no_contingency,unexpected_contingencies,completed,completed_tasks,charge_cycles,mission_cycles,total_dist,simulated_problems,traverse_time,scan_time,wait_time,cata_time,cont_time,charge_time,dock_time,undock_time\n")
                 out_file.write(name + "," + str(duration) + "," + str(self.expected_contingency_cnt) + "," + str(self.false_positive_contingency) + "," + str(self.false_negative_contingency)
                 + "," + str(self.issue_expected_without_contingy_and_fulfilled) + "," + str(self.unexpected_contingency_cnt) + "," + str(completed) + "," + str(self.completed_goals)
-                + "," + str(self.battery_charging_cycle + 1) + "," + str(self.mission_cycle) + "," + str(self.arox_total_dist) + "," + str(self.mode_times['traversing']) + "," 
-                + str(self.mode_times['scanning']) + "," + str(self.mode_times['waiting']) + "," + str(self.mode_times['catastrophe']) + "," + str(self.mode_times['contingency']) + ","
-                + str(self.mode_times['charging']) + "," + str(self.mode_times['docking']) + "," + str(self.mode_times['undocking']) + "\n")
+                + "," + str(self.battery_charging_cycle + 1) + "," + str(self.mission_cycle) + "," + str(self.arox_total_dist) + "," + str(self.simulated_problems) + ","
+                + str(self.mode_times['traversing']) + "," + str(self.mode_times['scanning']) + "," + str(self.mode_times['waiting']) + "," + str(self.mode_times['catastrophe']) + ","
+                + str(self.mode_times['contingency']) + "," + str(self.mode_times['charging']) + "," + str(self.mode_times['docking']) + "," + str(self.mode_times['undocking']) + "\n")
         except Exception as e:
             rospy.loginfo("EXCEPTION during storage of experiment results: %s", e)
 
@@ -282,12 +284,18 @@ class Experiment:
         rospy.loginfo("battery charge: %s, cycle: %s", self.battery_charge, self.battery_charging_cycle + 1)
         rospy.loginfo("mission cycle: %s", self.mission_cycle)
         rospy.loginfo("total distance: %sm", self.arox_total_dist)
+        rospy.loginfo("simulated problems: %s", self.simulated_problems)
         for i in self.mode_times.keys():
             rospy.loginfo("time in '%s' mode: %ss", i, self.mode_times[i])
         rospy.loginfo("###########################################################")
 
 def node():
     rospy.init_node('experiments')
+    
+    global TF_BUFFER
+    TF_BUFFER = tf2_ros.Buffer()
+    tf2_ros.TransformListener(TF_BUFFER)
+
     Experiment()
     rospy.loginfo("launch experiments node..")
     rospy.spin()
