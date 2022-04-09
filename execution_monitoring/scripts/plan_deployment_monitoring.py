97f13fdfcf8cfd89753a0a3960144ce159d8429d
tbohne <tbohne@uni-osnabrueck.de>
2022-04-09 17:48:19 +0200

    clean up plan deployment monitoring

--- execution_monitoring/scripts/plan_deployment_monitoring.py
+++ execution_monitoring/scripts/plan_deployment_monitoring.py
@@ -7,25 +7,39 @@ from datetime import datetime
 
 
 class PlanDeploymentMonitor:
+    """
+    Provides monitoring solutions for plan deployment failures.
+    """
 
     def __init__(self):
-        rospy.Subscriber(config.OPERATION_TOPIC, arox_operational_param, self.operation_callback, queue_size=1)
-        rospy.Subscriber('/plan_retrieval_failure', UInt16, self.plan_fail_callback, queue_size=1)
-        rospy.Subscriber('resolve_plan_deployment_failure_success', Bool, self.resolve_callback, queue_size=1)
+        self.last_op_time = datetime.now()
+
         self.aggravate_pub = rospy.Publisher('/aggravate', String, queue_size=1)
         self.contingency_pub = rospy.Publisher('/contingency_preemption', String, queue_size=1)
         self.robot_info_pub = rospy.Publisher('/robot_info', String, queue_size=1)
         self.interrupt_reason_pub = rospy.Publisher('/interrupt_reason', String, queue_size=1)
 
-        self.last_op_time = datetime.now()
+        rospy.Subscriber(config.OPERATION_TOPIC, arox_operational_param, self.operation_callback, queue_size=1)
+        rospy.Subscriber('/plan_retrieval_failure', UInt16, self.plan_fail_callback, queue_size=1)
+        rospy.Subscriber('resolve_plan_deployment_failure_success', Bool, self.resolve_callback, queue_size=1)
+
         self.start_monitoring()
 
     def resolve_callback(self, msg):
+        """
+        Resolver communication callback - determines whether resolution attempt was successful.
+        Aggravates to catastrophe in case of failure.
+
+        @param msg: callback message - True: resolution successful / False: resolution failed
+        """
         if not msg.data:
             self.interrupt_reason_pub.publish(config.PLAN_DEPLOYMENT_CATA)
             self.aggravate_pub.publish(config.PLAN_DEPLOYMENT_CATA)
 
     def start_monitoring(self):
+        """
+        Monitoring for plan deployment failures.
+        """
         while not rospy.is_shutdown():
             time_since_last_op = (datetime.now() - self.last_op_time).total_seconds()
             if time_since_last_op > config.IDLE_THRESH:
@@ -35,17 +49,32 @@ class PlanDeploymentMonitor:
             rospy.sleep(config.MON_FREQ)
 
     def operation_callback(self, operation_state):
-        # rospy.loginfo("state of operation: %s", operation_state)
+        """
+        Callback that receives information about the current state of the operation.
+        Only used to track the time of the last operational update.
+
+        @param operation_state: current operational parameters
+        """
         self.last_op_time = datetime.now()
-    
+
     def plan_fail_callback(self, msg):
+        """
+        Explicit plan deployment failures communicated by the operational state machine.
+
+        Error codes:
+        - 0 --> service timeout
+        - 1 --> empty plan
+        - 2 --> infeasible plan
+
+        @param msg: callback message - error code
+        """
         if msg.data == 0:
             rospy.loginfo("plan retrieval service timeout: %s", msg.data)
-            rospy.sleep(1) # need a delay for some reason
+            rospy.sleep(1)  # short delay
             self.contingency_pub.publish(config.PLAN_DEPLOYMENT_FAILURES[1])
         elif msg.data == 1:
             rospy.loginfo("empty plan: %s", msg.data)
-            rospy.sleep(1) # need a delay for some reason
+            rospy.sleep(1)  # short delay
             self.contingency_pub.publish(config.PLAN_DEPLOYMENT_FAILURES[2])
         elif msg.data == 2:
             rospy.loginfo("corrupted / infeasible plan: %s", msg.data)
@@ -56,10 +85,14 @@ class PlanDeploymentMonitor:
 
 
 def node():
+    """
+    Plan deployment failure monitoring node.
+    """
     rospy.init_node('plan_deployment_monitoring')
     PlanDeploymentMonitor()
     rospy.loginfo("launch plan deployment monitoring..")
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
