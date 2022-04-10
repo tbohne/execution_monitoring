f31a457eef086f7ce470273897ba3495f3879a01
tbohne <tbohne@uni-osnabrueck.de>
2022-04-10 14:46:58 +0200

    clean up sensor monitoring node

--- execution_monitoring/scripts/sensor_monitoring.py
+++ execution_monitoring/scripts/sensor_monitoring.py
@@ -7,29 +7,48 @@ import hashlib
 
 
 class SensorMonitoring:
+    """
+    Provides monitoring solutions for sensor failures.
+    """
 
     def __init__(self):
-        rospy.Subscriber('/scan_action', String, self.sensor_failure_monitoring, queue_size=1)
-        rospy.Subscriber('/resolve_sensor_failure_success', Bool, self.resolution_callback, queue_size=1)
+        self.previous_scan = None
         self.contingency_pub = rospy.Publisher('/contingency_preemption', String, queue_size=1)
         self.aggravate_pub = rospy.Publisher('/aggravate', String, queue_size=1)
         self.interrupt_reason_pub = rospy.Publisher('/interrupt_reason', String, queue_size=1)
-
-        self.previous_scan = None
+        rospy.Subscriber('/scan_action', String, self.sensor_failure_monitoring, queue_size=1)
+        rospy.Subscriber('/resolve_sensor_failure_success', Bool, self.resolution_callback, queue_size=1)
 
     def resolution_callback(self, msg):
+        """
+        Resolver communication callback - determines whether resolution attempt was successful.
+        Aggravates to catastrophe in case of failure.
+
+        @param msg: callback message - True: resolution successful / False: resolution failed
+        """
         if not msg.data:
             self.interrupt_reason_pub.publish(config.SENSOR_CATA)
             self.aggravate_pub.publish(config.SENSOR_CATA)
 
-    def compute_scan_hash(self, scan):
-        scan_str = str(scan.header.stamp.secs) + str(scan.header.stamp.nsecs) + scan.header.frame_id + str(scan.angle_min) \
-            + str(scan.angle_max) + str(scan.angle_increment) + str(scan.time_increment) + str(scan.scan_time) + str(scan.range_min) \
-            + str(scan.range_max) + str(scan.ranges)
+    @staticmethod
+    def compute_scan_hash(scan):
+        """
+        Computes a hash value that can be used to identify the specified scan.
 
+        @param scan: scan to compute hash for
+        @return: computed hash
+        """
+        scan_str = str(scan.header.stamp.secs) + str(scan.header.stamp.nsecs) + scan.header.frame_id \
+                   + str(scan.angle_min) + str(scan.angle_max) + str(scan.angle_increment) + str(scan.time_increment) \
+                   + str(scan.scan_time) + str(scan.range_min) + str(scan.range_max) + str(scan.ranges)
         return hashlib.sha224(scan_str.encode('utf-8')).hexdigest()
 
     def repeated_scan(self, scan):
+        """
+        Checks whether the specified scan has been recorded before.
+
+        @param scan: scan to be checked
+        """
         new_scan_hash = self.compute_scan_hash(scan)
         prev_scan_hash = self.compute_scan_hash(self.previous_scan)
         rospy.loginfo("new hash: %s", new_scan_hash)
@@ -37,6 +56,11 @@ class SensorMonitoring:
         return new_scan_hash == prev_scan_hash
 
     def sensor_failure_monitoring(self, msg):
+        """
+        Performs monitoring for sensor failures, initiated by scan action.
+
+        @param msg: callback message
+        """
         rospy.loginfo("start sensor monitoring..")
         scan = None
         try:
@@ -65,11 +89,15 @@ class SensorMonitoring:
 
 
 def node():
+    """
+    Sensor failure monitoring node.
+    """
     rospy.init_node('sensor_monitoring')
     rospy.wait_for_message('SMACH_running', String)
     rospy.loginfo("launch sensor monitoring..")
     SensorMonitoring()
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
